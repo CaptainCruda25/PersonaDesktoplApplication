7faa72f2b26d9ff7ea5e2b5bd79f83178e488fb7
captaincruda25 <crudacarlemmanuelt25@gmail.com>
2024-05-23 22:49:32 +0800

    Centralized desktop window

--- admindashboard.py
+++ admindashboard.py
@@ -5,4 +5,15 @@ import server
 
 
 window = Tk()
+window.title("Admin Dashboard")
+height = 650
+width = 550
+
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = (screen_width / 2) - (width / 2)
+y = (screen_height / 2) - (height / 2)
+window.geometry(f'{width}x{height}+{int(x)}+{int(y)}')
+
 window.mainloop()
--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,16 @@ from tkinter import messagebox
 
 window = Tk()
 window.title("Dashboard")
-window.geometry("4000x5000")
+height = 650
+width = 850
+
+screen_width = window.winfo_screenwidth() 
+screen_height = window.winfo_screenheight()
+
+x = (screen_width / 2) - (width / 2)
+y= (screen_height / 2) - (height / 2)
+
+window.geometry(f'{width}x{height}+{int(x)}+{int(y)}')
 window.configure(bg = "#333333")
 
 window.mainloop()
